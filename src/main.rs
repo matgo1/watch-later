527fd993f7fa8a2f76f3a32fa0d50829d72a5b9e
matgo1 <mathewstaran2707@gmail.com>
2026-07-05 13:33:29 +0300

    Added function to load josn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+const FILE_PATH: &str = "data/videos.json";
+
+// Template for json item
+#[derive(serde::Serialize, Deserialize)]
+pub struct Video {
+    title: String,
+    link: String,
+    description: Option<String>,
+}
+
+// Template of json file
+#[derive(Serialize, Deserialize)]
+pub struct JsonData {
+    videos: Vec<Video>,
+}
+
+// Class WatchLater
+pub struct WatchLater;
+
+impl WatchLater {
+    // Functionality:
+    // 1. add links to json
+    // 2. write random link from json
+    // 3. Optional! check if is valid link
+    // 4. Load json
+
+    pub fn load() -> Result<JsonData, Box<dyn std::error::Error>> {
+        let data = std::fs::read_to_string(FILE_PATH)?;
+        Ok(serde_json::from_str(&data)?)
+    }
+
+    pub fn check() {
+        println!("OK");
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    clearscreen::clear()?;
+    WatchLater::load()?;
+    WatchLater::check();
+    Ok(())
 }
