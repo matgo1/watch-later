b8391c53971fd3d31f1f4feafca28ad59b7ccb33
matgo1 <mathewstaran2707@gmail.com>
2026-07-19 18:10:25 +0300

    Wrote start bot polling

--- telegram_bot/src/tg_bot/main.py
+++ telegram_bot/src/tg_bot/main.py
@@ -13,8 +13,26 @@ async def run_bot():
     logger.info("Initializing Telegram Bot")
 
     # Construct Bot
-    bot = Bot(config.config.BOT_TOKEN)
+    bot = Bot(
+        config.config.BOT_TOKEN.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+    # Construct Dispatcher
     dp = Dispatcher()
+
+    # Include routers
+    # dp.include_router()
+
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+
+    except Exception as error:
+        logging.error(f"Error in starting bot polling: {error}")
+
+    finally:
+        await bot.session.close()
 
 
 def main():
